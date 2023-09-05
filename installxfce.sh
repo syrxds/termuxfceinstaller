81f10116f2e4d4f97deade0c84bc10bbcc73aaa1
antasod <tyolionosu@gmail.com>
2023-09-05 17:38:27 +0300

    Fix

--- installxfce.sh
+++ installxfce.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pkg update -y && clear && pkg upgrade -y && clear && pkg install x11-repo -y && clear && pkg install xfce4 tigervnc firefox -y && clear && vncserver && echo 'Done, please start xfce4 from the start.sh.'
+pkg update -y && clear && pkg upgrade -y && clear && pkg install x11-repo -y && clear && pkg install xfce4 tigervnc firefox -y && clear && echo 'Done, please start xfce4 from the start.sh.'
